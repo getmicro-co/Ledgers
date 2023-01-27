e31e875f08687663d91d1655340a9426fdbd80e4
Prathna Pandit <prathna.bt18@bmsce.ac.in>
2023-01-27 12:23:58 +0530

    added button

--- App.svelte
+++ App.svelte
@@ -2,6 +2,7 @@
   import { onMount } from "svelte";
   import Grid from "gridjs-svelte";
 
+  let current;
   let resData;
   let data = [];
 
@@ -25,12 +26,22 @@ name="searchbar"
 placeholder="Search..."
 /> -->
 
-<i class="mi mi-undo" on:click="{()=> {data = resData}}"></i>
+<!-- <i class="mi mi-undo" on:click="{()=> {data = resData}}"></i> -->
+
+<div class ="buttons">
+  <button class="{current === 'MTL' ? 'selected' : ''}"
+    on:click="{() => current = 'MTL'}">MTL</button>
+  <button class="{current === 'MAL' ? 'selected_two' : ''}"
+    on:click="{() => current = 'MAL'}" >MAL</button>
+  </div>
+  
+  <div class="refresh">
+  <i class="mi mi-undo" on:click="{()=> {data = resData}}"></i>
+  </div>
 
 <Grid data={data} 
  search  
  pagination={{ enabled:true, limit: 3 }}
- }}
 />
 
 <style>
@@ -63,5 +74,46 @@ placeholder="Search..."
     box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px,
       rgba(6, 24, 44, 0.65) 0px 4px 6px -1px,
       rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
+  }
+
+  button {
+    background-color: white;
+    color: black;
+    border: 2px solid #555555;
+    align-content: center;
+    padding: 8px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+    margin: 10px 3px;
+    transition-duration: 0.4s;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: #555555;
+    color: white;
+  }
+
+  .buttons {
+    text-align: center;
+  }
+
+  .selected {
+    background-color: rgb(245, 71, 71);
+    color: black;
+    font-weight: bold;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+  }
+  .selected_two {
+    background-color: rgb(243, 125, 7);
+    color: black;
+    font-weight: bold;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+  }
+
+  .refresh {
+    margin-right: 5px;
   }
 </style>
